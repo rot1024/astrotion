105d79843d677c36efbf03fc66b7723c774d1860
rot1024 <aayhrot@gmail.com>
2023-10-24 23:01:48 +0900

    chore: disable integration

--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -1,20 +1,22 @@
-import child_process from "node:child_process";
-import fs from "node:fs";
+// import child_process from "node:child_process";
+// import fs from "node:fs";
 
 import type { AstroIntegration } from "astro";
 
-const assetsDir = "public/static";
+// import { debug } from "../utils";
+
+// const assetsDir = ".astro/.astrotion/static";
+// const assetDirName = "static";
 
 export default (): AstroIntegration => ({
   name: "astrotion",
   hooks: {
-    "astro:build:done": async ({ dir }) => {
-      const outDir = new URL("notion", dir.href).pathname;
-      if (!fs.existsSync(outDir)) {
-        fs.mkdirSync(outDir);
-      }
-
-      child_process.execSync(`cp -n -r ${assetsDir}/* ${outDir} || true`);
+    "astro:build:done": async ({ dir: _ }) => {
+      // const outDir = new URL(assetDirName, dir.href).pathname;
+      // fs.mkdirSync(outDir, { recursive: true });
+      // const command = `cp -n -r ${assetsDir}/* ${outDir} || true`;
+      // debug(`copying assets: ${command}`);
+      // child_process.execSync(command);
     },
   },
 });
