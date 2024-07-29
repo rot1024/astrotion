5284120dd05f23441210d08bc761fcdbd56048cb
rot1024 <aayhrot@gmail.com>
2024-07-29 16:21:50 +0900

    fix: correct strong and em for cjk

---
+++ src/markdown.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+
+import { markdownToHTML } from "./markdown";
+
+test("markdown strong and em", async () => {
+  const md = "**「あああ！」**と__AAA！__と";
+  const html = await markdownToHTML(md);
+  expect
+    .soft(html)
+    .toBe(`<p><strong>「あああ！」</strong>と<strong>AAA！</strong>と</p>`);
+
+  const md2 = "_「あああ！」_と*A！*と";
+  const html2 = await markdownToHTML(md2);
+  expect.soft(html2).toBe(`<p><em>「あああ！」</em>と<em>A！</em>と</p>`);
+
+  const md3 = "**あああ**と";
+  const html3 = await markdownToHTML(md3);
+  expect.soft(html3).toBe(`<p><strong>あああ</strong>と</p>`);
+});
--- src/markdown.ts
+++ src/markdown.ts
@@ -3,6 +3,7 @@ import remarkEmbedder from "@remark-embedder/core";
 import type { Element, Text, Root } from "hast";
 import { h } from "hastscript";
 import isUrl from "is-url";
+import type { Root as MdRoot, Paragraph, PhrasingContent } from "mdast";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeKatex from "rehype-katex";
 import rehypeMermaid from "rehype-mermaid";
@@ -20,6 +21,7 @@ import { transformers } from "./embed";
 
 export const md2html = unified()
   .use(remarkParse)
+  .use(cjkEmphasis)
   .use(remarkGfm)
   .use(remarkMath)
   .use((remarkEmbedder as any).default as typeof remarkEmbedder, {
@@ -40,6 +42,46 @@ export const md2html = unified()
 
 export async function markdownToHTML(md: string): Promise<string> {
   return String(await md2html.process(md));
+}
+
+function cjkEmphasis() {
+  return (tree: MdRoot) => {
+    visit(tree, "text", (node) => {
+      const match = node.value.match(
+        /\*([^*]+)\*|\*\*([^*]+)\*\*|_([^*]+)_|__([^*]+)__/g,
+      );
+      if (!match) return;
+
+      const children = match.reduce<PhrasingContent[]>((acc, m) => {
+        const strongOrEm =
+          m.startsWith("**") || m.startsWith("__") ? "strong" : "emphasis";
+        const strongOrEmLen = strongOrEm === "strong" ? 2 : 1;
+        const text = m.slice(strongOrEmLen, -strongOrEmLen);
+        const index = node.value.indexOf(m);
+        if (index > 0) {
+          acc.push({
+            type: "text",
+            value: node.value.slice(0, index),
+          });
+        }
+        acc.push({
+          type: strongOrEm,
+          children: [{ type: "text", value: text }],
+        });
+        node.value = node.value.slice(index + m.length);
+        return acc;
+      }, []);
+
+      if (node.value.length > 0) {
+        children.push({ type: "text", value: node.value });
+      }
+
+      const pnode = node as unknown as Paragraph;
+      delete (node as any).value;
+      pnode.type = "paragraph";
+      pnode.children = children;
+    });
+  };
 }
 
 function autoLinkForFigcaption() {
